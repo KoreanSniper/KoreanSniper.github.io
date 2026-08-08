329ecfa998b24a3e0763845ffcf143743c16ad3a
KoreanSniper <seoul2linejh@gmail.com>
2026-08-08 17:09:43 +0900

    refactor: add shared authorization layer

---
+++ core/auth/permissions.js
@@ -0,0 +1,3 @@
+// Shared authorization helpers.
+// Migration-safe wrapper around the existing community utilities.
+export { isVerifiedGoogleUser, isAdminUser } from "../../../community/js/util.js";
